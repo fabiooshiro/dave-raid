98cfa6f67e747e0ec843bf291bfd61a76a155f31
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-05-19 12:31:20 -0300

    feat: add more questions

--- main.c
+++ main.c
@@ -36,8 +36,117 @@ void initialize_trivia(struct TriviaGame *game) {
         .answers = {question1Answers[0], question1Answers[1], question1Answers[2], question1Answers[3]},
         .correctAnswerIndex = 3
     };
+
+    struct TriviaAnswer question2Answers[NUM_ANSWERS] = {
+        {"Transparency"},
+        {"Immutability"},
+        {"Efficiency"},
+        {"Security"}
+    };
+    game->questions[1] = (struct TriviaQuestion) {
+        .question = "Which of the following is\na key characteristic\nof Blockchain technology?",
+        .answers = {question2Answers[0], question2Answers[1], question2Answers[2], question2Answers[3]},
+        .correctAnswerIndex = 1
+    };
+
+    struct TriviaAnswer question3Answers[NUM_ANSWERS] = {
+        {"Peer-to-peer"},
+        {"Centralized"},
+        {"Hierarchical"},
+        {"Decentralized"}
+    };
+    game->questions[2] = (struct TriviaQuestion) {
+        .question = "What type of network architecture\ndoes Blockchain technology utilize?",
+        .answers = {question3Answers[0], question3Answers[1], question3Answers[2], question3Answers[3]},
+        .correctAnswerIndex = 0
+    };
+
+    struct TriviaAnswer question4Answers[NUM_ANSWERS] = {
+        {"Smart contracts"},
+        {"Secure shell (SSH)"},
+        {"Social media"},
+        {"Supply chain management"}
+    };
+    game->questions[3] = (struct TriviaQuestion) {
+        .question = "Which application is NOT typically\nassociated with Blockchain technology?",
+        .answers = {question4Answers[0], question4Answers[1], question4Answers[2], question4Answers[3]},
+        .correctAnswerIndex = 1
+    };
+
+    struct TriviaAnswer question5Answers[NUM_ANSWERS] = {
+        {"Ethereum"},
+        {"Amazon Web Services (AWS)"},
+        {"Bitcoin"},
+        {"Hyperledger"}
+    };
+    game->questions[4] = (struct TriviaQuestion) {
+        .question = "Which platform is commonly\nused for deploying smart contracts?",
+        .answers = {question5Answers[0], question5Answers[1], question5Answers[2], question5Answers[3]},
+        .correctAnswerIndex = 0
+    };
+
+    struct TriviaAnswer question6Answers[NUM_ANSWERS] = {
+        {"Bitcoin"},
+        {"Litecoin"},
+        {"Ethereum"},
+        {"Ripple"}
+    };
+    game->questions[5] = (struct TriviaQuestion) {
+        .question = "Which cryptocurrency is known\nfor its focus on speed and low fees?",
+        .answers = {question6Answers[0], question6Answers[1], question6Answers[2], question6Answers[3]},
+        .correctAnswerIndex = 1
+    };
+
+    struct TriviaAnswer question7Answers[NUM_ANSWERS] = {
+        {"Proof of Work (PoW)"},
+        {"Proof of Stake (PoS)"},
+        {"Proof of Authority (PoA)"},
+        {"Proof of Concept (PoC)"}
+    };
+    game->questions[6] = (struct TriviaQuestion) {
+        .question = "Which consensus mechanism\nis used by Bitcoin?",
+        .answers = {question7Answers[0], question7Answers[1], question7Answers[2], question7Answers[3]},
+        .correctAnswerIndex = 0
+    };
+
+    struct TriviaAnswer question8Answers[NUM_ANSWERS] = {
+        {"Public key cryptography"},
+        {"Symmetric key cryptography"},
+        {"Hash functions"},
+        {"Digital signatures"}
+    };
+    game->questions[7] = (struct TriviaQuestion) {
+        .question = "Which cryptographic techniques\nare commonly used in Blockchain?",
+        .answers = {question8Answers[0], question8Answers[1], question8Answers[2], question8Answers[3]},
+        .correctAnswerIndex = 3
+    };
+
+    struct TriviaAnswer question9Answers[NUM_ANSWERS] = {
+        {"Immutable ledger"},
+        {"Dynamic database"},
+        {"Decentralized ledger"},
+        {"Centralized database"}
+    };
+    game->questions[8] = (struct TriviaQuestion) {
+        .question = "Which term describes the main\nfeature of a Blockchain?",
+        .answers = {question9Answers[0], question9Answers[1], question9Answers[2], question9Answers[3]},
+        .correctAnswerIndex = 0
+    };
+
+    struct TriviaAnswer question10Answers[NUM_ANSWERS] = {
+        {"Permissionless"},
+        {"Permissioned"},
+        {"Public"},
+        {"Private"}
+    };
+    game->questions[9] = (struct TriviaQuestion) {
+        .question = "What type of access control\nis commonly used in public\nBlockchain networks?",
+        .answers = {question10Answers[0], question10Answers[1], question10Answers[2], question10Answers[3]},
+        .correctAnswerIndex = 0
+    };
+
     game->selected = 0;
-    game->numQuestions = 1;
+    game->numQuestions = 10; // Update the total number of questions
     game->currentQuestionIndex = 0;
 }
 
@@ -197,6 +306,7 @@ void main() { // entry point
     }
     struct TriviaGame triviaGame;
     initialize_trivia(&triviaGame);
+    triviaGame.currentQuestionIndex = riv_rand_uint(triviaGame.numQuestions);
     do { // main loop
         if (game.trivia == 0) {
             run_game(&game);
@@ -206,6 +316,7 @@ void main() { // entry point
                 game.trivia = 0;
                 triviaGame.showRes = 0;
                 triviaGame.selected = 0;
+                triviaGame.currentQuestionIndex = riv_rand_uint(triviaGame.numQuestions);
             }
         }
     } while(riv_present()); // refresh screen and wait next frame
