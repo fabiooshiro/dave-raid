4b45ff3c84e5acb01a1b7cdc85de020edb811ff1
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-05-21 21:40:39 +0000

    feat: build to compile

---
+++ build.sh
@@ -0,0 +1,16 @@
+rm -rf main.sqfs
+
+wget -O rivemu https://github.com/rives-io/riv/releases/latest/download/rivemu-linux-amd64
+chmod +x rivemu
+
+mkdir -p $HOME/.riv
+mv rivemu $HOME/.riv/
+echo 'export PATH=$HOME/.riv:$PATH' >> $HOME/.bashrc
+export PATH=$HOME/.riv:$PATH
+
+wget -O rivos-sdk.ext2 https://github.com/rives-io/riv/releases/latest/download/rivos-sdk.ext2
+rivemu -no-window -sdk=rivos-sdk.ext2 -exec gcc --version
+
+rivemu -quiet -no-window -sdk -workspace -exec gcc main.c -o main -lriv
+rivemu -quiet -no-window -sdk -workspace -exec riv-strip main
+rivemu -quiet -no-window -sdk -workspace -exec riv-mksqfs main ship.png main.sqfs
